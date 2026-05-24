cd360520cad85ac31dde4cd599c3d808ad28aac9
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2026-05-25 00:26:50 +0530

    Update 18_control_flow.js

--- 18_control_flow.js
+++ 18_control_flow.js
@@ -143,3 +143,41 @@ if (loggedInFromGoogle || loggedInFromEmail) {
 // User logged in
 
 // Because one condition is true
+
+
+
+// ==========================================
+// 📘 Switch Case
+// ==========================================
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
